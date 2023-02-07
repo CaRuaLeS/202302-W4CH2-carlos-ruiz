ff43f6146b9602b94f494d356dfc354371ff2bac
CarlosRuiz <carua97@gmail.com>
2023-02-07 21:21:07 +0100

    Components added and forms files filled

---
+++ src/core/components/personal-data/personal-data.tsx
@@ -0,0 +1,27 @@
+export function PersonalData() {
+  return (
+    <form>
+      <div>
+        <input type="text" placeholder="Name" value="" />
+      </div>
+      <div>
+        <input type="text" placeholder="Last Name" value="" />
+      </div>
+      <div>
+        <input type="text" placeholder="Birth date" value="" />
+      </div>
+      <div>
+        <input type="radio-buton" placeholder="Gender" value="" />
+      </div>
+      <div>
+        <input type="text" placeholder="Email" value="" />
+      </div>
+      <div>
+        <input type="checkbox" placeholder="email address" value="" />
+      </div>
+      <div>
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+  );
+}
---
+++ src/core/components/user-form/user-form.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { AccesData } from "../acces-data/acces-data";
+import { Confirmation } from "../confirmation/confirmation";
+import { PersonalData } from "../personal-data/personal-data";
+
+export type PersonalData = {
+  name: string;
+  lastName: string;
+  birthDate: string;
+  gender: string;
+  email: string;
+  checkbox: boolean;
+};
+export type AccesData = {
+  username: string;
+  password: string;
+  repeatPassword: string;
+  accountType: string;
+};
+export type FormPage = {
+  step: number;
+};
+
+export type Data = PersonalData & AccesData & FormPage;
+
+export function UserForm() {
+  const [count, setPage] = useState(1);
+  const handlerClickPage = (incremento: number) => {
+    setPage(count + incremento);
+  };
+
+  switch (count) {
+    case 1:
+      return (
+        <>
+          <button onClick={() => handlerClickPage(+1)}>Next</button>
+          <PersonalData />
+        </>
+      ) as JSX.Element;
+    case 2:
+      return (
+        <>
+          <button onClick={() => handlerClickPage(-1)}>Previous</button>
+          <button onClick={() => handlerClickPage(+1)}>Next</button>
+          <AccesData />
+        </>
+      ) as JSX.Element;
+    case 3:
+      return (
+        <>
+          <button onClick={() => handlerClickPage(-1)}>Previous</button>
+          <Confirmation />
+        </>
+      ) as JSX.Element;
+    default:
+      return (
+        <>
+          <button onClick={() => handlerClickPage(+1)}>Next</button>
+          <PersonalData />
+        </>
+      );
+  }
+}
